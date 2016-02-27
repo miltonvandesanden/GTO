56d38088c08c5a26056dd80c1739ab0811a12cfa
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-02-27 20:45:20 +0100

    Star 1 mid-implementation

---
+++ Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public ResourceController resourceController;
+
+    public Text tbTurn;
+    public int turn;
+
+    // Use this for initialization
+    void Start ()
+    {
+        tbTurn.text = "Turn: " + turn;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        tbTurn.text = "Turn: " + turn;
+    }
+
+    public void endTurn()
+    {
+        turn += 1;
+
+        resourceController.endTurn();
+    }
+
+    public void spawnPokemon()
+    {
+        if (resourceController.balls > 0)
+        {
+            resourceController.balls -= 1;
+        }
+    }
+}
---
+++ Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResourceController : MonoBehaviour
+{
+    public Text tbBalls;
+    public Text tbPp;
+
+    public int balls;
+    public int pp;
+
+	// Use this for initialization
+	void Start()
+    {
+        tbBalls.text = "Balls: " + balls;
+        tbPp.text = "PP: " + pp;
+    }
+	
+	// FixedUpdate is called once per frame
+	void FixedUpdate()
+    {
+        tbBalls.text = "Balls: " + balls;
+        tbPp.text = "PP: " + pp;
+	}
+
+    public void endTurn()
+    {
+        pp += 2;
+    }
+}
