8e306b869b15c0e29913922f56056059272a22c8
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-03-09 14:43:31 +0100

    changed Attack

--- Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/Attack.cs
+++ Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/Attack.cs
@@ -3,20 +3,56 @@ using System.Collections;
 
 public class Attack
 {
-    public string name;
-    public string type;
-    public int power;
+    private string name;
+    private string type;
+    private int power;
+    private int accuracy;
 
-	// Use this for initialization
-/*	void Start(string name, string type, int power)
+    public Attack(string name, string type, int power, int accuracy)
     {
         this.name = name;
         this.type = type;
         this.power = power;
-    }*/
+        this.accuracy = accuracy;
+    }
 
-    public Attack()
+    public string getName()
     {
+        return name;
+    }
 
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    public string getType()
+    {
+        return type;
+    }
+
+    public void setType(string type)
+    {
+        this.type = type;
+    }
+
+    public int getPower()
+    {
+        return power;
+    }
+
+    public void setPower(int power)
+    {
+        this.power = power;
+    }
+
+    public int getAccuracy()
+    {
+        return accuracy;
+    }
+
+    public void setAccuracy(int accuracy)
+    {
+        this.accuracy = accuracy;
     }
 }
--- Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/PokemonController.cs
+++ Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/PokemonController.cs
@@ -3,29 +3,41 @@ using System.Collections;
 
 public class PokemonController : MonoBehaviour
 {
-    public int level;
-    public ArrayList attacks;
-    public string pokemonName;
+    public string name;
     public int health;
+    public int speed;
+    public ArrayList attacks = new ArrayList(4);
 
-	// Use this for initialization
-	void Start(string name)
+    // Use this for initialization
+    void Start(string name, int health, int speed, ArrayList attacks)
     {
         this.name = name;
-
-        if(name == "charmander")
-        {
-            //attacks.Add(new Attack("tackle", "normal", 50));
-        }
+        this.health = health;
+        this.speed = speed;
+        this.attacks = attacks;
     }
 	
-    public void attack(PokemonController enemyPokemon, Attack attack)
+    public bool attack(PokemonController otherPokemon, Attack attack)
     {
-        enemyPokemon.getAttacked(attack);
+        return otherPokemon.getAttacked(attack);
     }
 
-    public void getAttacked(Attack attack)
+    public bool getAttacked(Attack attack)
     {
+        bool hit = false;
 
+        if (Random.Range(1, 100) - attack.getAccuracy() > 0)
+        {
+            health -= attack.getPower();
+
+            hit = true;
+        }
+
+        if (health < 0)
+        {
+            health = 0;
+        }
+
+        return hit;
     }
 }
