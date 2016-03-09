0c7136d3500612dc5c89112625ae19769bf03c6a
Milton van de Sanden <milton.vandesanden@gmail.com>
2016-03-09 17:56:30 +0100

    stuff

--- Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/GameController.cs
+++ Week 1 - Resource  Management & Game Progress/Pokemon Battle Simulator/Assets/Scripts/GameController.cs
@@ -4,6 +4,49 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
+    public PlayerController player1;
+    public PlayerController player2;
+    public Transform blackTile;
+    public Transform whiteTile;
+
+    private int currentPlayer = 1;
+    private ArrayList tiles = new ArrayList();
+
+    public void Start()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                //if(x % 2 == 0)
+                //{
+                    if(y % 2 == 0)
+                    {
+                        Instantiate(whiteTile, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(blackTile, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                //}
+                //else
+                //{
+                    /*
+                    if (y % 2 == 0)
+                    {
+                        Instantiate(whiteTile, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(blackTile, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                    */
+                //}
+            }
+        }
+    }
+
+    /*
     public ResourceController resourceController;
     public Transform charmander;
 
@@ -38,4 +81,5 @@ public class GameController : MonoBehaviour
             Instantiate(charmander, new Vector3(resourceController.balls * 2.0f, 0, 0), Quaternion.identity);
         }
     }
+    */
 }
